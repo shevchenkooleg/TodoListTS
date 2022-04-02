b7c465e1a0e8a934e4762e6100ce925442cd8262
Oleg <shevchenko.oleg.87@gmail.com>
2022-04-02 20:13:46 +0300

    useState logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useReducer, useState} from 'react';
+import React, {useState} from 'react';
 import {v1} from 'uuid';
 import './App.css';
 import {MyUniversalInput} from "./universalComponent/MyUniversalInput";
